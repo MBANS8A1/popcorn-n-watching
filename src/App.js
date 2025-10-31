8ef3a81add99906a927864276a5d3ce4cbc1f2e9
MBANS8A1 <seanmbng@aol.com>
2025-10-31 20:41:31 +0000

    I extracted the input search element out of the NavBar component into a new SearchBar component, in case I need to reuse its functionality somewhere else.

--- src/App.js
+++ src/App.js
@@ -67,17 +67,23 @@ function NavBar() {
         <span role="img">🍿</span>
         <h1>usePopcorn</h1>
       </div>
-      <input
-        className="search"
-        type="text"
-        placeholder="Search movies..."
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
+
       <p className="num-results">
         Found <strong>X</strong> results
       </p>
     </nav>
+  );
+}
+
+function SearchBar() {
+  return (
+    <input
+      className="search"
+      type="text"
+      placeholder="Search movies..."
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+    />
   );
 }
 
