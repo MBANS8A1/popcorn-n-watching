e8098333c5ad280200c5edd51b993788b554e344
MBANS8A1 <seanmbng@aol.com>
2025-11-02 18:25:29 +0000

    I still want to user to have the ability to display text depending on whether they click or hover on the Star component; to do so I modified the text displayed on line 38 by adding another OR( ||) condition to check against.

--- src/StarRating.js
+++ src/StarRating.js
@@ -35,7 +35,7 @@ export default function StarRating({ maxRating = 5 }) {
           />
         ))}
       </div>
-      <p style={textStyle}>{tempRating || ""}</p>
+      <p style={textStyle}>{tempRating || rating || ""}</p>
     </div>
   );
 }
