cb9f047a1ab2c3280843f7145c15627047785852
MBANS8A1 <seanmbng@aol.com>
2025-11-02 09:10:42 +0000

    In the StarRating.js file, I added more objectsfor styling the text and the container for the stars.

--- src/StarRating.js
+++ src/StarRating.js
@@ -4,15 +4,25 @@ const containerStyle = {
   gap: "16px",
 };
 
+const starContainerStyle = {
+  display: "flex",
+  gap: "4px",
+};
+
+const textStyle = {
+  lineHeight: 1,
+  margin: 0,
+};
+
 export default function StarRating() {
   return (
     <div style={containerStyle}>
-      <div>
+      <div style={starContainerStyle}>
         {Array.from({ length: 5 }, (_, i) => (
           <span key={i}>S{i + 1}</span>
         ))}
       </div>
-      <p>10</p>
+      <p style={textStyle}>10</p>
     </div>
   );
 }
