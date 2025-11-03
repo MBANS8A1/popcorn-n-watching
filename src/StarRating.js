339c5be97ea4c9155a2e183966c5dada353f9a62
MBANS8A1 <seanmbng@aol.com>
2025-11-03 08:55:28 +0000

    I have tested out the StarRating component by calling it in the index.js file; the user can customize the color and the size of the stars as well as the maxRating. I made a correction as the colour property should have been spelt as color to specifically work with CSS rules.

--- src/StarRating.js
+++ src/StarRating.js
@@ -11,7 +11,7 @@ const starContainerStyle = {
 };
 
 export default function StarRating({
-  colour = "#fcc419",
+  color = "#fcc419",
   size = 48,
   maxRating = 5,
 }) {
@@ -21,7 +21,7 @@ export default function StarRating({
   const textStyle = {
     lineHeight: 1,
     margin: 0,
-    colour,
+    color,
     fontSize: `${size / 1.5}px`,
   };
 
@@ -38,7 +38,7 @@ export default function StarRating({
             full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
             onHoverIn={() => setTempRating(i + 1)}
             onHoverOut={() => setTempRating(0)}
-            colour={colour}
+            color={color}
             size={size}
           />
         ))}
@@ -48,7 +48,7 @@ export default function StarRating({
   );
 }
 
-function Star({ onRate, full, onHoverIn, onHoverOut, colour, size }) {
+function Star({ onRate, full, onHoverIn, onHoverOut, color, size }) {
   const starStyle = {
     width: `${size}px`,
     height: `${size}px`,
@@ -67,8 +67,8 @@ function Star({ onRate, full, onHoverIn, onHoverOut, colour, size }) {
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
-          fill={colour}
-          stroke={colour}
+          fill={color}
+          stroke={color}
         >
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
         </svg>
@@ -77,7 +77,7 @@ function Star({ onRate, full, onHoverIn, onHoverOut, colour, size }) {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke={colour}
+          stroke={color}
         >
           <path
             strokeLinecap="round"
--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    {/* <StarRating maxRating={5} /> */}
+    <StarRating size={24} color="red" />
   </React.StrictMode>
 );
