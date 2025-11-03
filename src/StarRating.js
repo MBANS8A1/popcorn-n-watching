022a7bacfb1e56bd4dceaa83c5b1f79e3121a26e
MBANS8A1 <seanmbng@aol.com>
2025-11-03 08:24:24 +0000

    I made an adjustment to the fontSize as the text was too big.

--- src/StarRating.js
+++ src/StarRating.js
@@ -22,7 +22,7 @@ export default function StarRating({
     lineHeight: 1,
     margin: 0,
     colour,
-    fontSize: `${size}`,
+    fontSize: `${size / 1.5}px`,
   };
 
   function handleRating(rating) {
