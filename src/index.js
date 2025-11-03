8a43a0e8580df371e59c784ae52957482bbad9bc
MBANS8A1 <seanmbng@aol.com>
2025-11-03 13:05:04 +0000

    I added the Test functional component in the index.js file, as I want to try allow the user/consumer to access the rating state outside of the StarRating component and within the Test component, where a message could be displayed.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,15 @@ import ReactDOM from "react-dom/client";
 // import App from "./App";
 import StarRating from "./StarRating";
 
+function Test() {
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} />
+      <p>This movie was rated X stars.</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
@@ -13,5 +22,6 @@ root.render(
       messages={["Terrible", "Bad", "Okay", "Good", "Spectacular"]}
     />
     <StarRating size={24} color="red" className="test" defaultRating={3} />
+    <Test />
   </React.StrictMode>
 );
