e214d828f3b2c510a2ac87e9705c512734eec01b
MBANS8A1 <seanmbng@aol.com>
2025-11-03 13:57:47 +0000

    In order to allow the user/consumer of the StarRating component to be able to output a custom <p> message, the rating state needs to be made accessible outside of the StarRating component and within the Test component. So, when a click/hover event on a Star occurs the rating value will be displayed via the Test component. To do this I am passing in the onSetRating prop as well a providing the movieRating useState hook.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./styles.css";
 // import App from "./App";
 import StarRating from "./StarRating";
 
 function Test() {
+  const [movieRating, setMovieRating] = useState(0);
   return (
     <div>
-      <StarRating color="blue" maxRating={10} />
-      <p>This movie was rated X stars.</p>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars.</p>
     </div>
   );
 }
